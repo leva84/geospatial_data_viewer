a4ddf8bd252277998b77c0529f1d5afc3ca2526d
Денис Левченко <denislevcenko@MacBook-Air-Denis.local>
2024-04-10 17:08:36 +0300

    Updated tests

--- spec/models/excavator_spec.rb
+++ spec/models/excavator_spec.rb
@@ -13,6 +13,16 @@ describe Excavator, type: :model do
     it { should validate_presence_of(:address) }
   end
 
+  context 'database columns' do
+    it { should have_db_column(:ticket_id).of_type(:integer).with_options(null: false) }
+    it { should have_db_column(:company_name).of_type(:string) }
+    it { should have_db_column(:address).of_type(:string) }
+    it { should have_db_column(:crew_on_site).of_type(:boolean) }
+    it { should have_db_column(:created_at).of_type(:datetime).with_options(null: false) }
+    it { should have_db_column(:updated_at).of_type(:datetime).with_options(null: false) }
+    it { should have_db_index(:ticket_id) }
+  end
+
   context 'saving to the database' do
     it 'persists to the database' do
       expect { subject.save }.to change(Excavator, :count).by(1)
--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -11,9 +11,21 @@ describe Ticket, type: :model do
     it { should validate_presence_of(:request_number) }
     it { should validate_presence_of(:sequence_number) }
     it { should validate_presence_of(:excavator) }
-
     it { should validate_uniqueness_of(:request_number) }
     it { should validate_uniqueness_of(:sequence_number) }
+  end
+
+  context 'database columns' do
+    it { should have_db_column(:request_number).of_type(:string) }
+    it { should have_db_column(:sequence_number).of_type(:string) }
+    it { should have_db_column(:request_type).of_type(:string) }
+    it { should have_db_column(:request_action).of_type(:string) }
+    it { should have_db_column(:response_due_date_time).of_type(:datetime) }
+    it { should have_db_column(:primary_service_area_code).of_type(:string) }
+    it { should have_db_column(:additional_service_area_codes).of_type(:string).with_options(default: [], array: true) }
+    it { should have_db_column(:well_known_text).of_type(:geography) }
+    it { should have_db_column(:created_at).of_type(:datetime).with_options(null: false) }
+    it { should have_db_column(:updated_at).of_type(:datetime).with_options(null: false) }
   end
 
   context 'saving to the database' do
