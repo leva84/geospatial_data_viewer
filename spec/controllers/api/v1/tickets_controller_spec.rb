e0a124035a54861e643fb7efac56ac909cab06c5
Денис Левченко <denislevcenko@MacBook-Air-Denis.local>
2024-04-13 18:07:48 +0300

    Update tests for Api::V1::TicketsController

--- spec/controllers/api/v1/tickets_controller_spec.rb
+++ spec/controllers/api/v1/tickets_controller_spec.rb
@@ -21,6 +21,32 @@ describe Api::V1::TicketsController, type: :controller do
 
     context 'with valid parameters' do
       let(:params) { { json_data: valid_ticket_json } }
+      let(:json_data_to_hash) { JSON.parse(params[:json_data]) }
+      let(:ticket) { Ticket.last }
+      let(:excavator) { ticket.excavator }
+      let(:well_known_text) { RGeo::Geographic.spherical_factory.parse_wkt(expected_ticket_data[:well_known_text]) }
+      let(:crew_on_site) { ActiveRecord::Type::Boolean.new.cast(expected_excavator_data[:crew_on_site]) }
+
+      let(:expected_ticket_data) do
+        {
+          request_number: json_data_to_hash['RequestNumber'],
+          sequence_number: json_data_to_hash['SequenceNumber'],
+          request_type: json_data_to_hash['RequestType'],
+          request_action: json_data_to_hash['RequestAction'],
+          response_due_date_time: json_data_to_hash.dig('DateTimes', 'ResponseDueDateTime'),
+          primary_service_area_code: json_data_to_hash.dig('ServiceArea', 'PrimaryServiceAreaCode', 'SACode'),
+          additional_service_area_codes: json_data_to_hash.dig('ServiceArea', 'AdditionalServiceAreaCodes', 'SACode'),
+          well_known_text: json_data_to_hash.dig('ExcavationInfo', 'DigsiteInfo', 'WellKnownText')
+        }
+      end
+
+      let(:expected_excavator_data) do
+        {
+          company_name: json_data_to_hash.dig('Excavator', 'CompanyName'),
+          address: json_data_to_hash.dig('Excavator', 'Address'),
+          crew_on_site: json_data_to_hash.dig('Excavator', 'CrewOnsite')
+        }
+      end
 
       it 'returns created status' do
         expect(subject).to have_http_status(:created)
@@ -33,6 +59,26 @@ describe Api::V1::TicketsController, type: :controller do
 
       it 'creates a new ticket and associated excavator in the database' do
         expect { subject }.to change(Ticket, :count).by(1).and change(Excavator, :count).by(1)
+      end
+
+      it 'creates record for ticket with expected values' do
+        subject
+        expect(ticket.request_number).to eq(expected_ticket_data[:request_number])
+        expect(ticket.sequence_number).to eq(expected_ticket_data[:sequence_number])
+        expect(ticket.request_type).to eq(expected_ticket_data[:request_type])
+        expect(ticket.request_action).to eq(expected_ticket_data[:request_action])
+        expect(ticket.response_due_date_time).to eq(expected_ticket_data[:response_due_date_time])
+        expect(ticket.primary_service_area_code).to eq(expected_ticket_data[:primary_service_area_code])
+        expect(ticket.additional_service_area_codes).to eq(expected_ticket_data[:additional_service_area_codes])
+        expect(ticket.well_known_text.to_s).to eq(well_known_text.to_s)
+      end
+
+      it 'creates record for excavator with expected values' do
+        subject
+        expect(excavator.company_name).to eq(expected_excavator_data[:company_name])
+        expect(excavator.address).to eq(expected_excavator_data[:address])
+        expect(excavator.crew_on_site).to eq(crew_on_site)
+        expect(excavator.ticket).to eq(ticket)
       end
     end
 
