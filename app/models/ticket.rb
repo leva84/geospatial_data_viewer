784328ce7f08092732c3a7ee88af4ddbc67553f5
Денис Левченко <denislevcenko@MacBook-Air-Denis.local>
2024-04-15 13:05:36 +0300

    Added related removal for excavator

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Ticket < ApplicationRecord
-  has_one :excavator
+  has_one :excavator, dependent: :destroy
 
   validates :request_number, :sequence_number, presence: true, uniqueness: true
   validates :well_known_text, presence: true
